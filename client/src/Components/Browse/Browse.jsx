fbb7aa6e7b400a762a47acaa2891a157b25717b6
Kathleen Hogan <kathleen.j.hogan@gmail.com>
2019-07-07 18:11:24 -0700

    Implement Browse component

---
+++ client/src/Components/Browse/Browse.jsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import axios from 'axios';
+import Navbar from '../Navbar/Navbar.jsx';
+import PostCard from './PostCard/PostCard.jsx';
+import './Browse.css';
+
+class Browse extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      posts: []
+    };
+    this.fetchPosts = this.fetchPosts.bind(this);
+  }
+
+  componentDidMount() {
+    this.fetchPosts();
+  }
+
+  fetchPosts() {
+    axios.get('/browse')
+      .then(({ data }) => this.setState({
+        posts: data
+      }))
+      .catch(err => console.log('Could not fetch posts: ', err));
+  }
+
+  render() {
+    const { username, avatar } = this.props.location.state;
+    return(
+      <div>
+        <Navbar username={username} avatar={avatar}/>
+        <h1 className="browse-h1">Recent Posts</h1>
+        <div className="browse-wrap">
+          <div className="four-col-grid">
+            {this.state.posts.map((post, i) => {
+              return <div className="grid-item hvr-grow" key={i}><PostCard post={post} username={username} avatar={avatar}/></div>               
+            })}
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Browse;
---
+++ client/src/Components/Browse/Postcard/Postcard.jsx
@@ -0,0 +1,55 @@
+import React from 'react';
+import './PostCard.css';
+import { FaCommentAlt, FaThumbsUp } from 'react-icons/fa';
+import moment from 'moment';
+
+class PostCard extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    const post = this.props.post;
+    const { username, avatar } = this.props;
+    // handling date display
+    let todaysDate = new Date();
+    let reviewDate = new Date(post.createdAt);
+    let daysBetween = (todaysDate.getTime() - reviewDate.getTime())/(1000*60*60*24.0);
+    let dateDisplay;
+    //display date differently if within past 7 days
+    if (daysBetween <= 7) {
+      dateDisplay = moment(post.createdAt).fromNow();
+    } else {
+      dateDisplay = moment(post.createdAt).format("MMM Do, YYYY");
+    }
+    return(
+      <div className="post-card">
+        <div className="post-card-user">
+          <img className="post-card-avatar" src={post.author.avatar}></img>
+          {/* INSERT USER PROFILE LINK BELOW */}
+          <a href="#">{post.author.username}</a>
+        </div>
+        <img className="post-card-img" src={post.image}></img>
+        <div className="post-card-description">
+          <h5>{post.title}</h5>
+          <p>{post.text.slice(0,100)}...</p>
+        </div>
+        <div className="post-card-details">
+          <p><strong>Restaurant:</strong> {post.restaurant}</p>
+          <div>
+            <div className="post-card-details-split">
+              <span><FaThumbsUp /> {post.likes}</span>
+              <span><FaCommentAlt className="post-card-comment-icon"/> {post.comments.length}</span>
+            </div>
+            <div className="post-card-details-split">
+              <p className="post-card-date">{dateDisplay}</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default PostCard;
