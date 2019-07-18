53355457d651fdcae3f127fddc671c5786e96df0
Kathleen Hogan <kathleen.j.hogan@gmail.com>
2019-07-17 23:30:52 -0700

    Begin handling comment submit on Post component

--- client/src/Components/Post/Post.jsx
+++ client/src/Components/Post/Post.jsx
@@ -11,9 +11,10 @@ class Post extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
+      post: this.props.post,
       author: {},
       comments: [], 
-      text: '', 
+      newComment: '', 
       like: false,
       showModal: false
     }
@@ -21,8 +22,9 @@ class Post extends React.Component {
     this.fetchAuthor = this.fetchAuthor.bind(this);
     this.handleShowModal = this.handleShowModal.bind(this);
     this.handleHideModal = this.handleHideModal.bind(this);
-    // this.handleChange = this.handleChange.bind(this);
-    // this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.getUpdatedPost = this.getUpdatedPost.bind(this);
     // this.handleLikePost = this.handleLikePost.bind(this);
   }
 
@@ -32,7 +34,7 @@ class Post extends React.Component {
   }
 
   fetchComments() {
-    const { id } = this.props.post;
+    const { id } = this.state.post;
     axios.get(`/posts/${id}/comments`)
     .then(({ data }) => {
       return data.sort((a, b) => {
@@ -48,7 +50,7 @@ class Post extends React.Component {
   }
 
   fetchAuthor() {
-    const id = this.props.post['author_id'];
+    const id = this.state.post['author_id'];
     axios.get(`/users/${id}/info`)
     .then(({ data }) => {
       this.setState({
@@ -70,21 +72,32 @@ class Post extends React.Component {
     });
   }
 
-  // handleChange (e) {
-  //   this.setState({
-  //     [e.target.name]: e.target.value
-  //   })
-  // }
+  handleChange(e) {
+    this.setState({
+      newComment: e.target.value
+    })
+  }
 
-  // handleSubmit (e) {
-  //   e.preventDefault()
-  //   const { text } = this.state;
-  //   const { id } = this.props.location;
-  //   const { username, avatar } = this.props.location;
-  //   axios.post('/comment', {text}, {params: {_id: id, username: username, avatar: avatar}})
-  //   .then(() => this.fetchOnePost())
-  //   .catch(() => console.error('Error with adding comment'))    
-  // }
+  handleSubmit(e) {
+    e.preventDefault()
+    const content = this.state.newComment;
+    const post_id = this.state.post.id;
+    const author_id = this.props.user.id;
+    axios.post('/comments', { author_id, post_id, content })
+    .then(() => this.getUpdatedPost())
+    .catch(err => console.log('Error posting comment: ', err));    
+  }
+
+  getUpdatedPost() {
+    const { id } = this.state.post;
+    axios.get(`/posts/${id}`)
+    .then(({ data }) => {
+      this.setState({
+        post: data
+      })
+    })
+    .catch(err => console.log('Error updating post: ', err));
+  }
   
   // handleLikePost () {
   //   const { id, username } = this.props.location;
@@ -108,7 +121,7 @@ class Post extends React.Component {
       (<AuthModal handleHide={this.handleHideModal} changeView={(option) => this.props.changeView(option)} changeUser={(user) => this.props.changeUser(user)}></AuthModal>) : null;
 
     const { username, avatar } = this.props.user;
-    const { created_at, img_url, recommended, restaurant, descript, title } = this.props.post;
+    const { created_at, img_url, recommended, restaurant, descript, title } = this.state.post;
     const { comments, author } = this.state;
     const commentSection = comments.length > 0 ? 
       <div className="show-post-comments">
@@ -123,9 +136,9 @@ class Post extends React.Component {
       <img className="post-recommend-img" src="https://res.cloudinary.com/kjhogan/image/upload/v1562452170/yumme_2_wphphq.png"></img>
     const likeIcon = this.state.like ? <FaThumbsUp className="post-like-icon-activated" /> : <FaThumbsUp className="post-like-icon" />;
     const commentBox = username ? 
-      <form>
-        {/* <textarea className="comment-input" id="comment-input" name="text" placeholder="Write a comment..." onChange={this.handleChange}/> */}
-        {/* <button type="submit" onClick={this.handleSubmit}><FaTelegramPlane /></button> */}
+      <form onSubmit={this.handleSubmit}>
+        <textarea className="comment-input" id="comment-input" name="newComment" placeholder="Write a comment..." onChange={this.handleChange}/>
+        <button type="submit" ><FaTelegramPlane /></button>
       </form> :
       <div className="cannot-comment">
         <p><em>Please </em> 
