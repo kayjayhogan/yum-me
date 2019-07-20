d7fbe029faa4a2bbae47dd26c0ec837855dc950b
Kathleen Hogan <kathleen.j.hogan@gmail.com>
2019-07-19 22:05:16 -0700

    Initialize form to create post

---
+++ client/src/Components/CreatePostForm/CreatePostForm.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './CreatePostForm.css';
+import Dropdown from './Dropdown/Dropdown.jsx';
+import NavBar from '../Navbar/Navbar.jsx'
+import axios from 'axios';
+
+class CreatePostForm extends React.Component {
+
+}
+
+export default CreatePostForm;
---
+++ client/src/Components/CreatePostForm/Dropdown/Dropdown.jsx
@@ -0,0 +1,3 @@
+import React from 'react';
+import './CreatePostForm.css';
+import Restaurant from './Restaurant.jsx';
