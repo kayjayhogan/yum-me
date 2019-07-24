b17c4e062cdd6c15090e0322cf7b65ade86b6426
Kathleen Hogan <kathleen.j.hogan@gmail.com>
2019-07-23 22:58:06 -0700

    Begin User page component

---
+++ client/src/Components/User/User.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import Navbar from '../Navbar/Navbar.jsx';
+import UserPost from './UserPost/UserPost.jsx';
+import './User.css';
+import axios from 'axios';
+import { MdLocationOn } from 'react-icons/md';
+
+class User extends React.Component {
+  constructor(props) {
+    this.state = {};
+  }
+
+  render() {
+    return(<div></div>);
+  }
+}
+
+export default User;
---
+++ client/src/Components/User/UserPost/UserPost.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './UserPost.css';
+import moment from 'moment';
+import { FaCommentAlt, FaThumbsUp } from 'react-icons/fa';
+
+const UserPost = (props) => {
+  return(<div></div>);
+}
+
+export default UserPost;
