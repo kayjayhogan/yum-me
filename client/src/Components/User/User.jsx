cecc8bdabbdfddacd9ea4a41c0e49918668205c1
Kathleen Hogan <kathleen.j.hogan@gmail.com>
2019-07-24 22:10:03 -0700

    Fetch user's posts

--- client/src/Components/User/User.jsx
+++ client/src/Components/User/User.jsx
@@ -7,11 +7,44 @@ import { MdLocationOn } from 'react-icons/md';
 
 class User extends React.Component {
   constructor(props) {
-    this.state = {};
+    super(props);
+    this.state = {
+      user: this.props.userPage,
+      posts: []
+    };
+    this.fetchUserPosts = this.fetchUserPosts.bind(this);
+  }
+
+  componentDidMount() {
+    this.fetchUserPosts();
+  }
+
+  fetchUserPosts() {
+    const { id } = this.state.user;
+    axios.get(`/users/${id}/posts`)
+    .then(({ data }) => {
+      this.setState({
+        posts: data
+      })
+    })
+    .catch(err => console.log("Error fetching user's posts: ", err));
   }
 
   render() {
-    return(<div></div>);
+    const { posts } = this.state;
+    const userPostSection = this.state.posts.length ? 
+      <div className="two-col-grid">
+        {posts.map((post, i) => <div key={i} className="grid-item hvr-grow"><UserPost post={post} /></div>)}
+      </div> : 
+      <div className="user-no-posts">
+        <h3>It looks like this user hasn't posted anything yet.</h3>
+        <div>
+          <p>Bummer!</p>
+          <img src="https://res.cloudinary.com/kjhogan/image/upload/v1536097829/terrible_ufki2y.png"></img>
+        </div>
+      </div>
+
+    return(<div>{userPostSection}</div>);
   }
 }
 
