fd5f0211e3fdffaad968da5a328616a4da2bde41
Kathleen Hogan <kathleen.j.hogan@gmail.com>
2019-07-25 20:43:41 -0700

    Begin Search component

---
+++ client/src/Components/Search/Search.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import axios from 'axios';
+import Navbar from '../Navbar/Navbar.jsx';
+import PostCard from '../Browse/PostCard/PostCard.jsx';
+import UserCard from './UserCard/UserCard.jsx';
+import './Search.css';
+
+class Search extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+}
+
+export default Search;
---
+++ client/src/Components/Search/UserCard/UserCard.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import axios from 'axios';
+import './UserCard.css';
+import { MdLocationOn } from 'react-icons/md';
+import { FaUsers } from 'react-icons/fa';
+
+class UserCard extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+} 
+
+export default UserCard;
