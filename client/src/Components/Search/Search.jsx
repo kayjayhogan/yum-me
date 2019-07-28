ad867037f5bb1237d48fa9a7ab9b46964d402973
Kathleen Hogan <kathleen.j.hogan@gmail.com>
2019-07-27 22:56:44 -0700

    Display search results for posts with matching title

--- client/src/Components/Search/Search.jsx
+++ client/src/Components/Search/Search.jsx
@@ -37,16 +37,45 @@ class Search extends React.Component {
   }
 
   render() {
+    // showing user matches
     let userList = this.state.usersMatch.length ? 
     this.state.usersMatch.map((user, i) => {
       return <div className="grid-item-five hvr-grow" key={i}><UserCard user={user} /></div>               
     }) : 
     <p className="result-p">No users found</p>;
+    // showing posts with title match
+    let postTitleList = this.state.postsTitleMatch.length ? 
+      this.state.postsTitleMatch.map((post, i) => {
+        return <div className="grid-item-four hvr-grow" key={i}><PostCard post={post} /></div>               
+      }) : 
+      <p className="result-p">No posts found</p>;
 
+    let users = this.state.usersMatch.length === 1 ? "user" : "users";
+    let postsT = this.state.postsTitleMatch.length === 1 ? "post" : "posts";
     return(
       <div>
         <Navbar username={this.props.user.username} avatar={this.props.user.avatar} changeView={(option) => this.props.changeView(option)} changeUser={(user) => this.props.changeUser(user)} handleSearchTerm={(term) => this.props.handleSearchTerm(term)}/>
-        {userList}
+        <div className="search">
+          <h1>Search Results for "{this.state.term}"</h1>
+          <div className="search-user-container">
+          {/* USERS */}
+          <div className="browse-wrap">
+            <h2><strong>{this.state.usersMatch.length}</strong> {users}</h2>
+              <div className="col-grid">
+                {userList}
+              </div>
+            </div>
+          </div>
+          <div>
+            {/* POSTS */}
+            <div className="browse-wrap">
+              <h2><strong>{this.state.postsTitleMatch.length}</strong> {postsT}</h2>
+              <div className="col-grid">
+                {postTitleList}
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
